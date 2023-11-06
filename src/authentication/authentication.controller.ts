10ea5978182cf2e8e3b0d2508cc0d8ac75ad027a
Joham Jaider Morales Franco <johamfranco318@gmail.com>
2023-11-06 15:31:01 -0500

    Peticiones para crear un usuario e iniciar usuario

--- src/authentication/authentication.controller.ts
+++ src/authentication/authentication.controller.ts
@@ -1,7 +1,38 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Req } from '@nestjs/common';
 import { AuthenticationService } from './authentication.service';
+import { Request } from 'express';
+import { Authentication_module } from '../modules/authentication_module';
+
+const authenticationModule = new Authentication_module();
 
 @Controller('authentication')
 export class AuthenticationController {
   constructor(private readonly authenticationService: AuthenticationService) {}
+
+  @Get()
+  GetAll(): string {
+    return 'Funciona';
+  }
+
+  @Post()
+  SignIn(/**@Res() res: Response,*/ @Req() req: Request) {
+    // console.log(req.body);
+    // const contraseña = req.body.User;
+    // res.status(HttpStatus.OK).json({
+    //   hola: 'holiss',
+    //   Contraseña: contraseña,
+    // });
+    return this.authenticationService.SignInUser(
+      req.body.email,
+      req.body.password,
+    );
+  }
+
+  @Post('register')
+  Register(@Req() req: Request) {
+    return this.authenticationService.CreateUser(
+      req.body.email,
+      req.body.password,
+    );
+  }
 }
--- src/authentication/authentication.service.ts
+++ src/authentication/authentication.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { Authentication_module } from '../modules/authentication_module';
+
+const authenticationModule = new Authentication_module();
 
 @Injectable()
-export class AuthenticationService {}
+export class AuthenticationService {
+  SignInUser(email: string, password: string) {
+    return authenticationModule.SignInUser(email, password);
+  }
+
+  CreateUser(email: string, password: string) {
+    return authenticationModule.CreateUser(email, password);
+  }
+}
